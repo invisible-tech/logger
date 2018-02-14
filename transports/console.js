e9f7c0a1917356e0f1dbff149f4e0e93330ac597
Leonardo Kewitz <leokewitz@gmail.com>
2018-02-14 11:45:38 -0500

    feat: Add Timber.io transport

--- transports/console.js
+++ transports/console.js
@@ -8,14 +8,26 @@ const assertLevel = require('./helpers/assertLevel')
 const {
   LOGGER_LEVEL = 'info',
   NODE_ENV,
+  TIMBER_KEY,
+  TIMBER_LEVEL = 'debug',
 } = process.env
 
 assertLevel(LOGGER_LEVEL, 'LOGGER_LEVEL invalid.')
 
 const colorize = NODE_ENV === 'development'
+const shouldLog = NODE_ENV !== 'test'
 
 let transport
-if (NODE_ENV !== 'test') {
+if (shouldLog && TIMBER_KEY) {
+  assertLevel(TIMBER_LEVEL, 'TIMBER_LEVEL invalid.')
+  const timber = require('timber')
+  timber.install(new timber.transports.HTTPS(TIMBER_KEY))
+  transport = new (winston.transports.Console)({
+    name: 'console',
+    level: TIMBER_LEVEL,
+    formatter: timber.formatters.Winston,
+  })
+} else if (shouldLog) {
   transport = new (winston.transports.Console)({
     name: 'console',
     level: LOGGER_LEVEL,
