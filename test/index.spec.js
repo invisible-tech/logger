16fcee329c34a1e9593914fda4e72f0f202f8223
Leonardo Kewitz <leokewitz@gmail.com>
2018-02-20 13:25:01 -0500

    test: Add Timber transport to test
    Also test different logging objects.

--- test/index.spec.js
+++ test/index.spec.js
@@ -12,9 +12,10 @@ describe('library interface', () => {
   process.env.BUGSNAG_KEY = 'fake'
   process.env.BUGSNAG_LEVEL = 'error'
   process.env.LOGGER_LEVEL = 'silly'
+  process.env.TIMBER_KEY = 'fake'
 
   it('it should load', () => {
-    logger = require('../index.js')
+    logger = require('../')
   })
 
   it('should load bugsnag transport if BUGSNAG_KEY is defined', () => {
@@ -23,5 +24,11 @@ describe('library interface', () => {
 
   it('should load bugsnag transport with the correct logging level', () => {
     assert.equal(logger.transports.bugsnag.level, process.env.BUGSNAG_LEVEL)
+  })
+
+  it('should work with strings, Errors and void messages', () => {
+    logger.error('daaamn', { meta: true })
+    logger.error(Error('daaamn'))
+    logger.error()
   })
 })
