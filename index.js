bf2ae2b6e7a1b4c5260b071596edccb0a723c5ab
Keenahn Tiberius Jung <keenahn@users.noreply.github.com>
2019-04-14 18:10:18 -0700

    feat: allow logging in tests if override in env vars

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const winston = require('winston')
 
 const isTest = (process.env.NODE_ENV === 'test')
+const enabledInTest = (process.env.LOGGER_ENABLED_IN_TEST === 'true')
 const transports = [
   (process.env.LOGGER_TIMBER === 'true') ?
     require('./transports/timber') :
@@ -11,7 +12,7 @@ const transports = [
 
 const logger = new (winston.Logger)({
   exitOnError: false,
-  transports: isTest ? undefined : transports,
+  transports: (isTest && ! enabledInTest) ? undefined : transports,
 })
 
 module.exports = logger
