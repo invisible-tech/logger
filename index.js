eac2ac2e5de379054dec366ea44575a058432c2e
Pieter Venter <pieter@venter.pro>
2020-03-21 15:05:47 +0700

    fix: using datavalues if toJSON not available

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 const winston = require('winston')
 
-const isTest = (process.env.NODE_ENV === 'test')
-const enabledInTest = (process.env.LOGGER_ENABLED_IN_TEST === 'true')
+const isTest = process.env.NODE_ENV === 'test'
+const enabledInTest = process.env.LOGGER_ENABLED_IN_TEST === 'true'
 const transports = [
-  (process.env.LOGGER_TIMBER === 'true') ?
-    require('./transports/timber') :
-    require('./transports/console'),
+  process.env.LOGGER_TIMBER === 'true'
+    ? require('./transports/timber')
+    : require('./transports/console'),
 ]
 
-const logger = new (winston.Logger)({
+const logger = new winston.Logger({
   exitOnError: false,
-  transports: (isTest && ! enabledInTest) ? undefined : transports,
+  transports: isTest && ! enabledInTest ? undefined : transports,
 })
 
 module.exports = logger
