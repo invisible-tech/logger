dec51a23d361aa966ee30cdd9bc98e5f5ac8d903
Pieter Venter <pieter@venter.pro>
2021-05-21 15:22:08 +0200

    refactor: switch to sentry.io

--- index.ts
+++ index.ts
@@ -1,19 +1,74 @@
-import winston, { createLogger } from 'winston'
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import * as Sentry from '@sentry/nextjs'
+import { captureException, captureMessage, Severity } from '@sentry/nextjs'
+import { Integrations } from '@sentry/tracing'
 
-import consoleTransport from './transports/console'
-import { rollbar } from './transports/rollbar'
+export const LOG_LEVEL = (process.env.LOG_LEVEL as Severity) ?? Severity.Warning
+export const SENTRY_DSN = process.env.SENTRY_DSN ?? process.env.NEXT_PUBLIC_SENTRY_DSN
 
-const isTest = process.env.NODE_ENV === 'test'
-const enabledInTest = process.env.LOGGER_ENABLED_IN_TEST === 'true'
-const transports = [consoleTransport]
-const { LOGGER_LEVEL = 'info', ROLLBAR_ACCESS_TOKEN } = process.env
+const SEVERITY_ARRAY = [
+  Severity.Debug,
+  Severity.Info,
+  Severity.Log,
+  Severity.Warning,
+  Severity.Error,
+  Severity.Critical,
+  Severity.Fatal,
+]
 
-const logger = ROLLBAR_ACCESS_TOKEN
-  ? rollbar
-  : createLogger({
-      level: LOGGER_LEVEL,
-      transports,
-      silent: isTest && !enabledInTest,
+export const { addBreadcrumb, addGlobalEventProcessor, captureEvent } = Sentry
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const init = () => {
+  if (SENTRY_DSN)
+    Sentry.init({
+      dsn: SENTRY_DSN,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      integrations: process['browser']
+        ? [new Integrations.BrowserTracing()]
+        : [new Integrations.Express()],
     })
+}
 
-export default logger as winston.Logger
+interface User extends Omit<Sentry.User, 'id'> {
+  id?: string | number
+  email?: string
+  name?: string
+}
+
+type Meta = Record<string, unknown>
+
+export const identify = (user: User) => {
+  if (!user.id && !user.email)
+    throw new Error('No email or id provided while calling identify in sentry lib')
+
+  Sentry.configureScope((scope) => {
+    scope.setUser(user as Sentry.User)
+  })
+}
+
+const logLevelIndex = SEVERITY_ARRAY.indexOf(LOG_LEVEL)
+const logMessage = (msg: string, severity: Severity = Severity.Info, meta: Meta = {}) => {
+  const severityIndex = SEVERITY_ARRAY.indexOf(severity)
+  if (meta instanceof Error) {
+    captureException(meta)
+    return
+  }
+  if (typeof meta.email === 'string') identify({ email: meta.email })
+  if (logLevelIndex >= severityIndex) {
+    addBreadcrumb({ level: severity, message: msg, data: meta })
+    return
+  }
+  captureMessage(msg, {
+    level: severity,
+    extra: meta,
+  })
+}
+
+export const logger = {
+  info: (msg: string, meta?: Meta) => logMessage(msg, Severity.Info, meta),
+  warn: (msg: string, meta?: Meta) => logMessage(msg, Severity.Warning, meta),
+  error: (msg: string, meta?: Meta) => logMessage(msg, Severity.Error, meta),
+  debug: (msg: string, meta?: Meta) => logMessage(msg, Severity.Debug, meta),
+}
--- types/winston-rollbar.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'winston-transport-rollbar-3'
