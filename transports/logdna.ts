226f37144ff9bcd3185d464c289f1b99bbe7d004
Keenahn Tiberius Jung <keenahn@users.noreply.github.com>
2021-08-17 19:38:14 -0400

    fix: added indexMeta: true to logdna logger

--- transports/logdna.ts
+++ transports/logdna.ts
@@ -2,6 +2,7 @@ import logdnaWinston from 'logdna-winston'
 
 const options = {
   key: process.env.LOGDNA_API_KEY,
+  indexMeta: true, // ensures that metadata is indexed in logdna
 }
 
 const logdna = new logdnaWinston(options)
